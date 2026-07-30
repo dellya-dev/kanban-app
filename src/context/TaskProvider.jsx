fe7f2ca8d889da13c68cfdfcbb7699c2d5865705
Dellya <dellyadellya061@gmail.com>
2026-07-30 18:19:23 +0700

    Add localStorage

--- src/context/TaskProvider.jsx
+++ src/context/TaskProvider.jsx
@@ -1,9 +1,26 @@
-import { useReducer } from "react"
+import { useEffect, useReducer } from "react"
 import { initialState, taskReducer } from "../reducers/taskReducer"
 import { TaskContext } from "./TaskContext"
 
 function TaskProvider( {children} ) {
-  const [state, dispatch] = useReducer(taskReducer, initialState)
+  const [state, dispatch] = 
+  useReducer(
+    taskReducer, 
+    initialState,
+    (initial) => {
+      const saved = localStorage.getItem("tasks")
+      if (saved) {
+        const parsed = JSON.parse(saved)
+        return parsed
+      } else {
+        return initial
+      }
+    })
+
+    useEffect(() => {
+      localStorage.setItem("tasks", JSON.stringify(state))
+    }, [state])
+
 
   return(
     <TaskContext.Provider 
