44bc027e4f2839403e9b10fec7c0704e33b7f8b8
Dellya <dellyadellya061@gmail.com>
2026-08-04 10:07:08 +0700

    Add CSS responsive

--- src/components/FilterBar.jsx
+++ src/components/FilterBar.jsx
@@ -1,26 +1,57 @@
+import { useState } from 'react'
 import './FilterBar.css'
 
 function FilterBar({ setFilter }) {
+  const [isOpen, setIsOpen] = useState(false)
 
-  return(
+  return (
     <>
       <div className='filter-container'>
-        <button 
-        className='all-button'
+        <button
+          className='all-button'
           onClick={() => setFilter("all")}
         >All</button>
-        <button 
-        className='low-button'
+        <button
+          className='low-button'
           onClick={() => setFilter("low")}
         >Low</button>
         <button
-          className='medium-button' 
+          className='medium-button'
           onClick={() => setFilter("medium")}
         >Medium</button>
         <button
-          className='high-button' 
+          className='high-button'
           onClick={() => setFilter("high")}
         >High</button>
+      </div>
+      <div>
+        <div className="mobile-menu">
+          <button
+            className='mobile-menu-icon'
+            onClick={() => setIsOpen(!isOpen)}
+          >☰</button>
+
+          {isOpen && (
+            <div className="mobile-dropdown">
+              <button
+                className='all-button-mobile'
+                onClick={() => setFilter("all")}
+              >All</button>
+              <button
+                className='low-button-mobile'
+                onClick={() => setFilter("low")}
+              >Low</button>
+              <button
+                className='medium-button-mobile'
+                onClick={() => setFilter("medium")}
+              >Medium</button>
+              <button
+                className='high-button-mobile'
+                onClick={() => setFilter("high")}
+              >High</button>
+            </div>
+          )}
+        </div>
       </div>
     </>
   )
